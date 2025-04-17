e4a15d0e0fc4b50050f96cac14a8d5bf848cc48e
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-04-17 23:51:09 +0530

    built lognPage ♠

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+    return(
+        <div className= "w-full bg-red-700 h-screen">
+            <h1>
+                Login Page
+            </h1>
+        </div>
+    )
+}
