ec8cf7b666c6597cf81b212697ded08cea72c56e
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-04-18 23:20:46 +0530

    component update 🛠

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './pages/loginPage'
+import ProductCard from './components/product-card'
+import Header from './components/header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <LoginPage />
+  <>
+    <Header/>
+  </>
+    //<LoginPage />
   )
 }
 
