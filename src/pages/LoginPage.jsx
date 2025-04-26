207436a1c5509085e560cba9ca5f70302b24a8bc
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-04-26 19:52:28 +0530

    login page background 🔧

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,12 @@
 export default function LoginPage(){
     return(
-        <div className= "w-full bg-red-700 h-screen">
-            <h1>
-                Login Page
-            </h1>
+        <div className= "w-full bg-red-700 h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center flex">
+         <div className=" w-[50%] h-full"></div> 
+         <div className=" w-[50%] h-full flex items-center justify-center">
+            <div className=" w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl"></div>
+          </div>  
+           
+          
         </div>
     )
 }
