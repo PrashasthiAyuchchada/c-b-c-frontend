dd056e9c317ec4798265441ae8c4bb9e556f7c96
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-04-26 18:48:33 +0530

    div modified 🔨

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,12 @@
 
 
 export default function Header() {
-  
-
   return (
-    <header >
-     <h1>My Store</h1>
+    <header className="w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-[100px] flex items-center justify-center shadow-lg">
+      <h1 className="text-4xl font-bold tracking-wide text-white drop-shadow-md">
+        My Store
+      </h1>
     </header>
   );
 }
+
