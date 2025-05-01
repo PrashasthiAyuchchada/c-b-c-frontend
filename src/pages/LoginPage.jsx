7c9399bda900956cc1a985f1a90591c519f284f3
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-05-01 22:04:13 +0530

    dynamic credential (email , password ) from useState ,   Input - onchange  , button- onclick  and axios installed ⚙🛠⛓

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
+
 export default function LoginPage(){
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    function handleLogin(){
+        console.log("email:",email);
+        console.log("password:",password);
+        console.log("Login button clicked");
+    }
+
+
     return(
         <div className= "w-full bg-red-700 h-screen bg-[url(/login-bg.jpg)] bg-cover bg-center flex">
          <div className=" w-[50%] h-full"></div> 
          <div className=" w-[50%] h-full flex items-center justify-center">
+
+
             <div className=" w-[450px] h-[600px] backdrop-blur-xl shadow-xl rounded-xl flex flex-col items-center justify-center">
-                <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" type="email" placeholder="email"/>
-                <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px] " type="password" placeholder="password"/>
-                <button className="w-[400px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Login</button>
+                <input onChange={
+                     (e)=>{
+                        setEmail(e.target.value);
+                     }
+                }  
+                className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px]" type="email" placeholder="email"/>
+                <input onChange={
+                    (e)=>{
+                        setPassword(e.target.value);
+                    }
+                }
+                className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[5px] " type="password" placeholder="password"/>
+                <button onClick={handleLogin} className="w-[400px] h-[50px] bg-green-500 text-white rounded-xl cursor-pointer">Login</button>
             </div>
           </div>  
            
