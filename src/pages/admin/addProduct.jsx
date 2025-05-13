f739c7ddd5881b33e79e04c7734e3cdff1970f48
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-05-14 01:24:59 +0530

    Add_Product Form🛠

--- src/pages/admin/addProduct.jsx
+++ src/pages/admin/addProduct.jsx
@@ -18,6 +18,7 @@
 
 
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function AddProductForm(){
 
@@ -25,27 +26,39 @@ export default function AddProductForm(){
     return(
         <div className="w-full h-full rounded-2xl flex flex-col items-center justify-center"> 
             <div className="w-[500px] h-[600px] bg-white rounded-lg shadow-lg flex flex-col items-center">
+                <h1 className="text-3xl font-bold text-gray-700 m-[10px]">ADD PRODUCT</h1>
                 <input 
                 className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
                 placeholder="Product ID"
                 />
                  <input 
                 className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
-                placeholder="Product ID"
+                placeholder="Product Name"
                 />
                  <input 
                 className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
-                placeholder="Product ID"
+                placeholder="Alternative Names"
                 />
                  <input 
                 className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
-                placeholder="Product ID"
+                placeholder="Price"
                 />
                  <input 
                 className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
-                placeholder="Product ID"
+                placeholder="Labeled Price"
                 />
-                
+                 <textarea 
+                className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
+                placeholder="Description"
+                />
+                 <input 
+                className="w-[400px] h-[50px] border border-gray-500 rounded-xl text-center m-[5px]" 
+                placeholder="Stock"
+                />
+                <div className="w-[400px] h-[100px] flex justify-between items-center rounded-lg">
+                    <Link to={"/admin/products"} className="bg-red-500 text-white text-center p-[10px] rounded-lg w-[180px] h-[50px] hover:bg-red-600 cursor-pointer " >Cancel</Link>
+                    <button   className="bg-green-500 text-white text-center p-[10px] rounded-lg w-[180px] h-[50px] hover:bg-green-600 cursor-pointer m-2">Submit</button>
+                </div>
          
 
 
