949d390a78a94fe7ec7a873bf6c2f86d92dacbb0
Prashasthi Ayuchchada Parakrama <khprashasthi@gmail.com>
2025-05-21 19:33:50 +0530

    setup file uplaod , unique name for each uploaded file for supabase ⚙

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,20 +1,47 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { createClient } from '@supabase/supabase-js';
+
 
 export default function TestPage(){
-
+    const [file, setFile] = useState(null)
+    const supabase = createClient("https://mbbqqmbehcrvrehmfgfd.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iYnFxbWJlaGNydnJlaG1mZ2ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NzY2NzYsImV4cCI6MjA2MzI1MjY3Nn0.sHRgn3JxSaZ5NdY2jso1kp24dXHWS_gS4n3y2QtmL9k")
     
-    const [count,setCount] = useState(0)
-    const [status ,setStatus] = useState("Passed")
+    function handleUpload(){
+        const fileName = file.name
+        const newFileName = new Date().getTime()+fileName
+
+
+        supabase.storage.from("images").upload(newFileName, file, {
+            cacheControl: "3600",
+            upsert: false,
+        }).then(
+            ()=>{
+                toast.success("File upload successfully")
+                const url = supabase.storage.from("images").getPublicUrl(newFileName).data.publicUrl
+                console.log(url)
+            }
+            
+        ).catch(
+            ()=>{
+                toast.error("File upload failed")
+            }
+        )
+
+        
+
+    }
 
 
     return(
         <div className=" w-full h-screen  flex flex-col items-center justify-center"> 
-            <input type = "file" onChange ={
+            <input type = "file" 
+            onChange ={
                 (e)=>{
-                   console.log (e.target.files)
+                   setFile(e.target.files[0]);
                 }
             }/>
-
+            <button onClick={handleUpload} className="bg-gray-700 text-white p-2 rounded-lg ">Upload</button>
            
            
         </div>
